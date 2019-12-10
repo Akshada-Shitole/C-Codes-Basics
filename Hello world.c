c35c02d4fac3f01f07b3dcfe7427144ecaa875f4
Akshada-Shitole <58739761+Akshada-Shitole@users.noreply.github.com>
2019-12-10 22:44:28 +0530

    Add files via upload

---
+++ Hello world.c
@@ -0,0 +1,10 @@
+    //Write a program to display hello world in C
+    #include <stdio.h>
+    int main()
+    {
+       // printf() displays the string inside quotation
+       printf("Hello, World!");
+       return 0;
+    }
+
+    //OUTPUT:Hello, World!
